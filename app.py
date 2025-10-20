efb687642efb4345a4099e9c2556ef78155a1b36
ant6924 <danthony082706@gmail.com>
2025-10-20 16:37:02 -0400

    Refactor imports and update function references
    Updated imports and fixed function references for LangChain and Streamlit compatibility.

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ import bcrypt
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_classic.chains.retrieval import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_huggingface import HuggingFaceEmbeddings
 
